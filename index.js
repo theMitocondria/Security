0f125cfe2ea1ddea9f84daceecebd7ebbbb63045
theMitocondria <100942663+theMitocondria@users.noreply.github.com>
2022-12-23 14:26:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ passport.deserializeUser(function(user, done) { done(null, user) });
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/secrets"
+    callbackURL: "https://securitylogin.onrender.com/auth/google/secrets"
   },
   function(accessToken, refreshToken, profile, cb) {
     console.log(profile);
